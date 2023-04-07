80c077cfaff07134fe48d87be1fb10d57631aa53
Aadhith-Ujesh <ujeshm2k2@gmail.com>
2023-04-07 09:21:11 +0530

    ml model added

--- Backend/app.py
+++ Backend/app.py
@@ -5,6 +5,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 run_with_ngrok(app)
 app = Flask(__name__)
+import numpy as np
+model = pickle.load(open('ML model/Untitled7.pickle', 'rb'))
+arr = np.zeros(113)
 app.secret_key = 'Tahve bqltuyej tbrjereq qobfd MvIaTq cmanmvpcuxsz iesh tihkel CnTu dretpyauritompeanstd '
 CORS(app,origins='*')
 
@@ -17,6 +20,32 @@ def fetchData():
     curLat = request.json['Latitude']
     curLong = request.json['Longitude']
     weatherCondition = request.json['weatherCondition']
+    if(Aircraft_damage == 'Destroyed'):
+      arr[0] = 1
+    if(Aircraft_damage == 'Minor'):
+      arr[1] = 1
+    if(Aircraft_damage == 'Substantial'):
+      arr[2] = 1  
+    if(Weather_Condition == 'IMC'):
+      arr[3] = 1
+    if(Weather_Condition == 'UNK'):
+      arr[4] = 1
+    if(Weather_Condition == 'VMC'):
+      arr[6] = 1
+    if(Aircraft_Category == 'Airplane'):
+      arr[7] = 1
+    if(Aircraft_Category == 'Helicopter'):
+      arr[9] = 1
+    if(Make == "Boeing"):
+      arr[37] = 1
+    if(Make == "CESSNA"):
+      arr[38] = 1
+    
+    from sklearn.preprocessing import MinMaxScaler
+    from sklearn import preprocessing
+
+    min_max_scaler = preprocessing.MinMaxScaler()
+    arr_scaled = min_max_scaler.fit_transform(arr)
     
     lat,long = get_destination(curLat,curLong)
     returner = {{-lat,long},{lat,-long},{lat,long},{-lat,-long}}
